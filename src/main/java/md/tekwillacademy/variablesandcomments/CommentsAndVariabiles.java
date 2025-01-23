76038c9172b14f3ab7d04de70cb954f5897ebb71
Ecaterina Bacalim <ecaterina.bacalim@endava.com>
2025-01-23 15:15:34 +0200

    added two new variables as a practical exercise

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariabiles.java
@@ -0,0 +1,17 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariabiles {
+    public static void main(String[] args) {
+        boolean isHotOutside;
+        isHotOutside = true;
+        System.out.println("Is it hot outside: "+ isHotOutside);
+        boolean isItFriday = false;
+        System.out.println("Is it Friday: " + isItFriday);
+
+    }
+
+
+
+
+
+}
